d7ba98120c894ac293dc850c85ad4cb865e34e02
Tanish Narang <narangtanish2@gmail.com>
2024-08-31 17:34:39 +0530

    creating card of the contact

--- src/component/ContactCard.js
+++ src/component/ContactCard.js
@@ -1,3 +1,20 @@
-export default function ContactCard() {
-  return <div>ContactCard</div>;
-}
+const ContactCard = ({ contact }) => {
+  const { id, name, email } = contact;
+
+  return (
+    <div className="item">
+      <div className="content">
+        <div className="header">{name}</div>
+        <div>{email}</div>
+        <div>
+          <i
+            className="trash alternate outline icon"
+            style={{ color: "red", marginTop: "7px", float: "right" }}
+          ></i>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactCard;
