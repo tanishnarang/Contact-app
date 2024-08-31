687af19bc5fb997a35afc2ba79e66c4de2fb1a03
Tanish Narang <narangtanish2@gmail.com>
2024-08-31 17:33:01 +0530

    listing the default contacts

--- src/component/ContactList.js
+++ src/component/ContactList.js
@@ -1,3 +1,10 @@
-export default function ContactList() {
-  return <div>ContactList</div>;
-}
+import React from "react";
+import ContactCard from "./ContactCard";
+const ContactList = ({ contacts }) => {
+  const renderedContactList = contacts.map((contact) => {
+    return <ContactCard contact={contact} />;
+  });
+  return <div className="ui celled list">{renderedContactList}</div>;
+};
+
+export default ContactList;
