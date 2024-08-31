be225975f2e8b1296b1edede90cc1aeaee805096
Tanish Narang <narangtanish2@gmail.com>
2024-08-31 17:04:48 +0530

    created form for adding contact

--- src/component/AddContact.js
+++ src/component/AddContact.js
@@ -1,3 +1,32 @@
-export default function AddContact() {
-  return <div>AddContact</div>;
+import React from "react";
+
+class AddContact extends React.Component {
+  render() {
+    return (
+      <div classNames="ui main">
+        <h2>Add Contact</h2>
+        <form className="ui form">
+          <div className="field">
+            <label>Name</label>
+            <input
+              type="text"
+              name="name"
+              placeholder="Type Your Name.."
+            ></input>
+          </div>
+          <div className="field">
+            <label>Email</label>
+            <input
+              type="email"
+              name="name"
+              placeholder="example@gmail.com"
+            ></input>
+            <button className="ui button blue">Add</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
+
+export default AddContact;
