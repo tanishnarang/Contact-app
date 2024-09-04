66c48ee31633782ad4efe424d4c5495e75bd77d3
Tanish Narang <narangtanish2@gmail.com>
2024-09-05 05:20:31 +0530

    An alert before deleting contact

--- src/component/ContactCard.js
+++ src/component/ContactCard.js
@@ -1,8 +1,30 @@
 import user from "../images/user.png";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const ContactCard = ({ contact, clickHandler }) => {
   const { id, name, email } = contact;
+
+  const handleDelete = () =>
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#2185D0",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        clickHandler(id);
+        Swal.fire({
+          title: "Deleted!",
+          text: "contact has been deleted.",
+          icon: "success",
+          ButtonColor: "#2185D0",
+        });
+      }
+    });
 
   return (
     <div className="item flex items-center">
@@ -16,9 +38,7 @@ const ContactCard = ({ contact, clickHandler }) => {
       <i
         className="trash alternate outline icon ml-auto cursor-pointer"
         style={{ color: "red", float: "right" }}
-        onClick={() => {
-          clickHandler(id);
-        }}
+        onClick={handleDelete}
       ></i>
     </div>
   );
